be5141d1b7fc4ee37bd6dbc2cb116937189b18d0
Bobby Bates <robertfbatesjr@gmail.com>
2022-09-04 10:00:23 -0400

    complete assignment

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -9,8 +9,29 @@ class MoviesController < ApplicationController
   def show
     movie = Movie.find(params[:id])
     render json: movie
+
+    # Either choose what to include:
+    # render json: movie.to_json(only: [:id, :title, :year, :length, :director, :description, :poster_url, :category, :discount, :female_director])
+
+    # Or what to exclude:
+    # render json: movie.to_json(except: [:created_at, :updated_at])
+
+    # Include nested resource:
+    # render json: post.to_json(only: [:title, :description, :id], include: [author: { only: [:name]}])
+
+    # But! It's better to separate concerns and use a Serializer @ movie_serializer.rb
   end
 
+  def summary
+    movie = Movie.find(params[:id])
+    render json: movie, serializer: MovieSummarySerializer
+  end
+
+  def summaries
+    movies = Movie.all
+    render json: movies, each_serializer: MovieSummarySerializer
+  end
+  
   private
 
   def render_not_found_response
